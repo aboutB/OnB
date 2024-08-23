5723d8ecb0d27537627b2aec49b979e0dcada3e9
aboutB <zeloesbj@gmail.com>
2024-08-23 20:59:25 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            navLinks.forEach(nav => nav.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+
+
+
+
+});
